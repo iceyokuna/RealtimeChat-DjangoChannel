2fae92c0a4400556cd9b7a3a26049492859dcd73
Iceyo Kuna <iceyokuna@hotmail.com>
2018-10-07 22:24:27 +0700

    Add project
    - Add project Web-Socket Django Channels

---
+++ chatApp/ChatModule/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChatmoduleConfig(AppConfig):
+    name = 'ChatModule'
---
+++ chatApp/ChatModule/consumers.py
@@ -0,0 +1,78 @@
+
+from channels.generic.websocket import WebsocketConsumer
+import json
+import requests
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        print('web client connected')
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        bot_res = requests.get('http://127.0.0.1:8001',json={'message':message})
+
+        message = 'YOU : ' + message
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+        bot_msg = 'BOT : '+ str(bot_res.json()['message']) + '\n'
+        self.send(text_data=json.dumps({
+            'message': bot_msg
+        }))
+
+
+'''
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+    '''
